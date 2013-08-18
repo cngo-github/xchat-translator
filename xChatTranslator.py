f2883a9483827dab9b4b11c104527c33167baa02
Chuong Ngo <chuong.h.ngo.ctr@mail.mil>
2013-08-18 17:26:55 -0400

    Merged Karona's pull request

--- xChatTranslator.py
+++ xChatTranslator.py
@@ -3,7 +3,7 @@
 __module_name__ = "translator"
 __module_version__ = "0.1"
 __module_description__ = "Translates from one language to other using Google Translate via YQL."
-__module_author__ = "drag,karona75"
+__module_author__ = "drag, karona75"
 
 import xchat
 
@@ -14,23 +14,21 @@ DEST_LANG = 'en'
 AUTOUSER = {}
 
 def getPage(words,src,dest):
-	#url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22this%20is%20a%20test%22%20and%20source%3D%22en%22%20and%20target%3D%22de%22%3B&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback="
 	url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22%D0%92%D1%81%D0%B5%D0%BC%20%D0%A5%D0%B0%D0%B9,%20%D0%BF%D0%B0%D1%86%D0%B0%D0%BD%D1%8B%22%20and%20target%3D%22en%22%20and%20source%3D%22ru%22%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-	#url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22" + words +"%22%20and%20target%3D%22" + dest + "%22%20and%20source%3D%22" + src +"%22	3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
 	headers = { 'User-Agent' : 'Mozilla/5.0' }
 	req = urllib2.Request(url, None, headers)
 	response = urllib2.urlopen(req)
 	return response.read()
 
 def parseJsonResult(resultStr):
-	result =resultStr
+	result = resultStr
 	resultArray = result['query']['results']['json']['json'][0]['json']
-	str1=""
+	retStr=""
 	if type(resultArray) is dict:
 		str1+=resultArray['json'][0]
 	else:
 		for subDict in resultArray:
-			str1+=subDict['json'][0]
+			retStr += subDict['json'][0]
 	return str1
 
 def translateTo(word, word_eo1, userdata):
