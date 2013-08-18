34ef44a547cab0627b58cb9f5073205f09719d4a
Chuong Ngo <chuong.h.ngo.ctr@mail.mil>
2013-08-18 18:41:38 -0400

    Added translate, translateTo, and translateFrom - Chuong Ngo

--- xChatTranslator.py
+++ xChatTranslator.py
@@ -13,8 +13,106 @@ import urllib2
 DEST_LANG = 'en'
 AUTOUSER = {}
 
-def getPage(words,src,dest):
-	url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22%D0%92%D1%81%D0%B5%D0%BC%20%D0%A5%D0%B0%D0%B9,%20%D0%BF%D0%B0%D1%86%D0%B0%D0%BD%D1%8B%22%20and%20target%3D%22en%22%20and%20source%3D%22ru%22%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+class translator:
+	LANGUAGES = {
+		'AFRIKAANS' : 'af',
+	        'ALBANIAN' : 'sq',
+	        'AMHARIC' : 'am',
+	        'ARABIC' : 'ar',
+	        'ARMENIAN' : 'hy',
+	        'AZERBAIJANI' : 'az',
+	        'BASQUE' : 'eu',
+	        'BELARUSIAN' : 'be',
+	        'BENGALI' : 'bn',
+	        'BIHARI' : 'bh',
+	        'BULGARIAN' : 'bg',
+	        'BURMESE' : 'my',
+	        'CATALAN' : 'ca',
+	        'CHEROKEE' : 'chr',
+	        'CHINESE' : 'zh',
+	        'CHINESE_SIMPLIFIED' : 'zh-CN',
+	        'CHINESE_TRADITIONAL' : 'zh-TW',
+	        'CROATIAN' : 'hr',
+	        'CZECH' : 'cs',
+	        'DANISH' : 'da',
+	        'DHIVEHI' : 'dv',
+	        'DUTCH': 'nl',  
+	        'ENGLISH' : 'en',
+	        'ESPERANTO' : 'eo',
+	        'ESTONIAN' : 'et',
+	        'FILIPINO' : 'tl',
+	        'FINNISH' : 'fi',
+	        'FRENCH' : 'fr',
+	        'GALICIAN' : 'gl',
+	        'GEORGIAN' : 'ka',
+	        'GERMAN' : 'de',
+	        'GREEK' : 'el',
+	        'GUARANI' : 'gn',
+	        'GUJARATI' : 'gu',
+	        'HEBREW' : 'iw',
+	        'HINDI' : 'hi',
+	        'HUNGARIAN' : 'hu',
+	        'ICELANDIC' : 'is',
+	        'INDONESIAN' : 'id',
+	        'INUKTITUT' : 'iu',
+	        'IRISH' : 'ga',
+	        'ITALIAN' : 'it',
+	        'JAPANESE' : 'ja',
+	        'KANNADA' : 'kn',
+	        'KAZAKH' : 'kk',
+	        'KHMER' : 'km',
+	        'KOREAN' : 'ko',
+	        'KURDISH': 'ku',
+	        'KYRGYZ': 'ky',
+	        'LAOTHIAN': 'lo',
+	        'LATVIAN' : 'lv',
+	        'LITHUANIAN' : 'lt',
+	        'MACEDONIAN' : 'mk',
+	        'MALAY' : 'ms',
+	        'MALAYALAM' : 'ml',
+	        'MALTESE' : 'mt',
+	        'MARATHI' : 'mr',
+	        'MONGOLIAN' : 'mn',
+	        'NEPALI' : 'ne',
+	        'NORWEGIAN' : 'no',
+	        'ORIYA' : 'or',
+	        'PASHTO' : 'ps',
+	        'PERSIAN' : 'fa',
+	        'POLISH' : 'pl',
+	        'PORTUGUESE' : 'pt-PT',
+	        'PUNJABI' : 'pa',
+	        'ROMANIAN' : 'ro',
+	        'RUSSIAN' : 'ru',
+	        'SANSKRIT' : 'sa',
+	        'SERBIAN' : 'sr',
+	        'SINDHI' : 'sd',
+	        'SINHALESE' : 'si',
+	        'SLOVAK' : 'sk',
+	        'SLOVENIAN' : 'sl',
+	        'SPANISH' : 'es',
+	        'SWAHILI' : 'sw',
+	        'SWEDISH' : 'sv',
+	        'TAJIK' : 'tg',
+	        'TAMIL' : 'ta',
+	        'TAGALOG' : 'tl',
+	        'TELUGU' : 'te',
+	        'THAI' : 'th',
+	        'TIBETAN' : 'bo',
+	        'TURKISH' : 'tr',
+	        'UKRAINIAN' : 'uk',
+	        'URDU' : 'ur',
+	        'UZBEK' : 'uz',
+	        'UIGHUR' : 'ug',
+	        'VIETNAMESE' : 'vi',
+	        'WELSH' : 'cy',
+	        'YIDDISH' : 'yi'
+	}
+
+def getPage(message, src, dest):
+	url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22" + message + "%22%20and%20target%3D%22" + dest + "%22%20and%20source%3D%22" + src + "%22%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+
+# %D0%92%D1%81%D0%B5%D0%BC%20%D0%A5%D0%B0%D0%B9,%20%D0%BF%D0%B0%D1%86%D0%B0%D0%BD%D1%8B%
+	print url
 	headers = { 'User-Agent' : 'Mozilla/5.0' }
 	req = urllib2.Request(url, None, headers)
 	response = urllib2.urlopen(req)
@@ -23,28 +121,61 @@ def getPage(words,src,dest):
 def parseJsonResult(resultStr):
 	result = resultStr
 	resultArray = result['query']['results']['json']['json'][0]['json']
-	retStr=""
+	retStr = ""
+
 	if type(resultArray) is dict:
-		str1+=resultArray['json'][0]
+		retStr += resultArray['json'][0]
 	else:
 		for subDict in resultArray:
 			retStr += subDict['json'][0]
-	return str1
 
-def translateTo(word, word_eo1, userdata):
+	retStr = retStr.encode("utf-8")
+	return retStr
+
+def translate(word, word_eol, userdata):
 	print "Starting translation"
-	srcStr = "this is just a test"
-	destLanguage = "ru"
-	srcLanguage = "en"
+	
+	srcLang = word[1]
+	destLang = word[2]
+	message = word_eol[3]
 
-	print "Running translation"
-	page = getPage(urllib2.quote(srcStr.encode('utf8')), srcLanguage, destLanguage)
+	uMessage = unicode(message, "utf-8" )
+	page = getPage(urllib2.quote(message), srcLang, destLang)
 	result = json.loads(page)
-	print "Result got translation"
-	translated = parseJsonResult(result)
 
-	print translated
-	xchat.command('say Translation complete.')
+	xchat.prnt("Translated: " + parseJsonResult(result))
 	return xchat.EAT_ALL
 
-xchat.hook_command("tr", translateTo, help="/tr <message> translates message into the language specified.")
+xchat.hook_command("tr", translate, help="/tr <source language> <target language> <message> translates message into the language specified.")
+
+def translateTo(word, word_eol, userdata):
+	print "Starting translation"
+	
+	srcLang = DEST_LANG
+	destLang = word[1]
+	message = word_eol[2]
+
+	uMessage = unicode(message, "utf-8" )
+	page = getPage(urllib2.quote(message), srcLang, destLang)
+	result = json.loads(page)
+
+	xchat.command('say ' + parseJsonResult(result))
+	return xchat.EAT_ALL
+
+xchat.hook_command("trt", translateTo, help="/trt <target language> <message> translates message into the language specified and sends it as a /say command.")
+
+def translateFrom(word, word_eol, userdata):
+	print "Starting translation"
+	
+	srcLang = word[1]
+	destLang = DEST_LANG
+	message = word_eol[2]
+
+	uMessage = unicode(message, "utf-8" )
+	page = getPage(urllib2.quote(message), srcLang, destLang)
+	result = json.loads(page)
+
+	xchat.prnt('Translated from ' + srcLang + ': ' + parseJsonResult(result))
+	return xchat.EAT_ALL
+
+xchat.hook_command("trf", translateFrom, help="/trf <source language> <message> translates message into the language specified.")
