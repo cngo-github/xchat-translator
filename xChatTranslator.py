b2786635d25678c291ceb15f9443b438546e3c96
Chuong Ngo <chuong.h.ngo.ctr@mail.mil>
2013-08-18 18:46:29 -0400

    Removed unnecessary debugging print out

--- xChatTranslator.py
+++ xChatTranslator.py
@@ -111,8 +111,6 @@ class translator:
 def getPage(message, src, dest):
 	url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22" + message + "%22%20and%20target%3D%22" + dest + "%22%20and%20source%3D%22" + src + "%22%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
 
-# %D0%92%D1%81%D0%B5%D0%BC%20%D0%A5%D0%B0%D0%B9,%20%D0%BF%D0%B0%D1%86%D0%B0%D0%BD%D1%8B%
-	print url
 	headers = { 'User-Agent' : 'Mozilla/5.0' }
 	req = urllib2.Request(url, None, headers)
 	response = urllib2.urlopen(req)
